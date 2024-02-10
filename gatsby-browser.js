dae032e65caaca9c52e8d8d1b784e269512a0ced
Josh Terrill <josh@hacked.codes>
2024-02-09 18:08:23 -0800

    fixed styling on code samples

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,6 +7,6 @@ import "./src/normalize.css";
 import "./src/style.css";
 
 // Highlighting for code blocks
-import "prismjs/themes/prism.css";
+import "prismjs/themes/prism-okaidia.css";
 import "prismjs/plugins/line-numbers/prism-line-numbers.css";
 import "prismjs/plugins/command-line/prism-command-line.css";
